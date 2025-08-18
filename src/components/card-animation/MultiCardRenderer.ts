4b9e57d4ba11fb43800a1d9a8e79d2dedd78744f
Pavan Koka <pavankurmarao.k@arrise.com>
2025-08-18 13:44:40 +0530

    changes

--- src/components/card-animation/MultiCardRenderer.ts
+++ src/components/card-animation/MultiCardRenderer.ts
@@ -279,7 +279,7 @@ export class MultiCardRenderer {
         // Textures: align handling with CardRenderer
         this.backTexture = this.loadBackTexture('/images/card-back.jpg');
         // this.frontTexture = this.loadImageTexture('/images/queen-hearts.avif');
-        this.frontTexture = this.loadImageTexture('/images/queen-hearts.avif');
+        this.frontTexture = this.loadImageTexture('/images/ace-hearts.png');
 
         // GL state (mirror CardRenderer)
         this.gl.useProgram(this.program);
